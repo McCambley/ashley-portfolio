bc01a1c40b0fefc1370d67738cce4e174ac4b336
McCambley <jakemccamb16@gmail.com>
2023-12-04 23:05:19 -0500

    Add fonts and update layout

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Nothing_You_Could_Do, Raleway, Lato } from 'next/font/google';
+
+export const nothingYouCouldDo = Nothing_You_Could_Do({
+  subsets: ['latin'],
+  weight: '400',
+});
+export const raleway = Raleway({ subsets: ['latin'] });
+export const lato = Lato({
+  subsets: ['latin'],
+  weight: '400',
+});
