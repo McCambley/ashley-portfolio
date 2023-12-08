b9fdacbb0a9b154a9d22d21c213c4f90d916f616
McCambley <jakemccamb16@gmail.com>
2023-12-08 01:53:14 -0500

    Update font imports in fonts.ts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,11 +1,27 @@
-import { Inter, Nothing_You_Could_Do, Raleway, Lato } from 'next/font/google';
+import {
+  Sacramento as Handwriting,
+  Sacramento as Heading,
+  Space_Mono as Body,
+  Space_Mono as Mono,
+} from 'next/font/google';
 
-export const nothingYouCouldDo = Nothing_You_Could_Do({
+export const handwriting = Handwriting({
   subsets: ['latin'],
+  weight: '400',
+  variable: '--font-handwriting',
+});
+export const heading = Heading({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-heading',
+});
+export const body = Body({
+  subsets: ['latin'],
+  variable: '--font-body',
   weight: '400',
 });
-export const raleway = Raleway({ subsets: ['latin'] });
-export const lato = Lato({
+export const mono = Mono({
   subsets: ['latin'],
   weight: '400',
+  variable: '--font-mono',
 });
