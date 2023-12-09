337214f0160aebac9a5c353661a52160ecf6b4af
McCambley <jakemccamb16@gmail.com>
2023-12-09 02:05:17 -0500

    Add contact form functionality

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    console.log({ event });
+    const { name, value } = event.target;
+    console.log({ formValues });
+    setFormValues(prevFormValues => ({
+      ...prevFormValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section>
+      <h1>Contact Me</h1>
+      <p>
+        Looking for a new designer for your team? Someone to team up with? Or
+        someone to bounce ideas off of? Let me know!
+      </p>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formValues.message}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 }
