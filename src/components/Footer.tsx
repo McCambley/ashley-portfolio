32d601fb811d970e25ac83ae258b7e9b72aaf1fe
McCambley <jakemccamb16@gmail.com>
2023-12-09 02:04:44 -0500

    Add image and links to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,50 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Footer() {
   return (
     <section>
-      <h2 className="font-heading w-full text-center">Placeholder</h2>
       <div className="mb-8 flex flex-col items-center">
-        <p className="font-body text-3xl gradient-text">
-          I&apos;m currently looking for new opportunities. If you&apos;d like
-          to get in touch, please reach out to me at{' '}
-          <a
-            href="mailto:test@test.com"
-            className="underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            My Email
-          </a>{' '}
-          or on{' '}
-          <a
-            href="https://www.linkedin.com/in/ashley-fife/"
-            className="underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          .
-        </p>
+        <Image
+          src="/ashley_nh.png"
+          width={100}
+          height={100}
+          alt="Ashley's Headshot"
+        />
+        <div>
+          <p>Ashley Fife Design</p>
+          <p>UI/UX Designer</p>
+        </div>
       </div>
-      {/* A container containing three buttons that are all call to actoins. "Hire me" "View Work" and "Contact Me" */}
+      {/* A container containing three Links that are all call to actoins. "Hire me" "View Work" and "Contact Me" */}
       <div className="flex justify-center">
-        <button className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow">
-          Hire Me
-        </button>
-        <button className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow">
-          View Work
-        </button>
-        <button className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow">
-          Contact Me
-        </button>
+        <Link
+          href="/about"
+          className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow"
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow"
+        >
+          Contact
+        </Link>
+        <Link
+          href="/resume"
+          className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow"
+        >
+          Resume
+        </Link>
+        <Link
+          href={'/freelance'}
+          className="max-w-[22rem] rounded-full border-4 border-af-yellow px-8 py-3 text-3xl text-af-yellow"
+        >
+          Freelance
+        </Link>
       </div>
+      <Image src={'/linkedin.svg'} width={80} height={80} alt="linkedin" />
     </section>
   );
 }
