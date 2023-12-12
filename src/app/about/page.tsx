1fb2376fdaef83f11ba3ec960fe2bdc034614d6c
McCambley <jakemccamb16@gmail.com>
2023-12-12 08:55:38 -0500

    Refactor about page layout and add gradient border

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import Skills from '@/components/Skills';
 import Image from 'next/image';
+import GradientBorder from '@/components/GradientBorder';
 
 const paragraphs = [
   `When I finished my bachelors degree in Counseling Psychology, I longed to go to do unconventional jobs in unconventional places. I spent a decade doing seasonal manual labor in New Hampshire, Greenland, Alaska, and Antarctica in the pursuit of land stewardship and supporting science.`,
@@ -11,32 +12,36 @@ export default function About() {
   return (
     <div>
       <section>
-        <h1>About Me</h1>
-        <div>
-          {paragraphs.map(paragraph => (
-            <p key={paragraph}>{paragraph}</p>
-          ))}
-        </div>
-        <div>
-          <Image
-            src={'/ashley_ak.png'}
-            alt="Ashley in Alaska"
-            width={200}
-            height={200}
-          />
-          <Image
-            src={'/ashley_greenland.png'}
-            alt="Ashley in Alaska"
-            width={200}
-            height={200}
-          />
-          <Image
-            src={'/ashley_ice.png'}
-            alt="Ashley in Alaska"
-            width={200}
-            height={200}
-          />
-        </div>
+        <GradientBorder className="flex flex-col items-center rounded-lg p-10">
+          <h1>About Me</h1>
+          <div className="flex flex-col justify-between md:flex-row">
+            <div className="w-1/2 gradient-text">
+              {paragraphs.map(paragraph => (
+                <p key={paragraph}>{paragraph}</p>
+              ))}
+            </div>
+            <div className="flex w-1/2 justify-center">
+              <Image
+                src={'/ashley_ak.png'}
+                alt="Ashley in Alaska"
+                width={200}
+                height={200}
+              />
+              <Image
+                src={'/ashley_greenland.png'}
+                alt="Ashley in Alaska"
+                width={200}
+                height={200}
+              />
+              <Image
+                src={'/ashley_ice.png'}
+                alt="Ashley in Alaska"
+                width={200}
+                height={200}
+              />
+            </div>
+          </div>
+        </GradientBorder>
       </section>
       <Skills />
     </div>
