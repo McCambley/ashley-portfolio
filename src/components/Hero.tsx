62fad22993e0a33d8d1d7f2d0d34fa716c872531
McCambley <jakemccamb16@gmail.com>
2023-12-12 21:16:15 -0500

    Update Hero component styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         <h1 className="mb-4 w-full text-center font-handwriting md:mb-auto md:text-left">
           Hi, I&apos;m Ashley
         </h1>
-        <GradientBorder className="mx-auto mb-6 w-fit rounded-full md:hidden">
+        <GradientBorder className="mx-auto mb-6 w-3/4 rounded-full md:hidden">
           <Image
             src={ashleyHeadshot.src}
             height={400}
@@ -18,7 +18,7 @@ function Hero() {
             className="rounded-full md:hidden"
           />
         </GradientBorder>
-        <p className="font-body text-2xl gradient-text">
+        <p className="text-center font-body text-2xl gradient-text md:text-left">
           I&apos;m a UI/UX designer with a love for blending beauty and
           functionality. I aim to bring vibrant design and intuitive user
           experience to all facets of the human experience.
