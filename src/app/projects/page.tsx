3f7081b4f3e6746760e8e9d71d854abfd8a1c339
McCambley <jakemccamb16@gmail.com>
2023-12-13 00:11:10 -0500

    Refactor Projects component to use ProjectsComponent

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,22 +1,8 @@
 import Link from 'next/link';
+import ProjectsComponent from '@/components/Projects';
 
 function Projects() {
-  return (
-    <div>
-      <h1>Projects</h1>
-      <ul>
-        <li>
-          <Link href="/projects/city-cycles">City Cycles</Link>
-        </li>
-        <li>
-          <Link href="/projects/midnight-brew">Midnight Brew</Link>
-        </li>
-        <li>
-          <Link href="/projects/sit-conmigo">Sit Conmigo</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <ProjectsComponent />;
 }
 
 export default Projects;
