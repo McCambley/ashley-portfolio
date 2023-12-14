0cc5db4776fa026cdb1cb533ddd79538d6e1cdb8
McCambley <jakemccamb16@gmail.com>
2023-12-14 02:10:42 -0500

    Add ContactForm component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Projects from '@/components/Projects';
 import Hero from '@/components/Hero';
+import ContactForm from '@/components/ContactForm';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects />
+      <ContactForm
+        className="mb-16 max-w-[calc(800px+64px)]"
+        showSubtitle={true}
+      />
     </>
   );
 }
