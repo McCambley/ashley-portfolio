995c944c261fb2b94cfa8395ddcb8bd09dbdac55
McCambley <jakemccamb16@gmail.com>
2023-12-15 00:15:16 -0500

    Add hero image to City Cycles page

--- src/app/projects/city-cycles/page.tsx
+++ src/app/projects/city-cycles/page.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import heroImage from '~/case_studies/city_cycles/city_cycle_hero_image.png';
 
 function CityCycles() {
   return (
     <div>
+      <Image
+        src={heroImage.src}
+        height={heroImage.height}
+        width={heroImage.width}
+      ></Image>
       <h1>City Cycles</h1>
     </div>
   );
