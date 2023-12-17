df191391e675522174f4188c2b566e9e9648a2c4
McCambley <jakemccamb16@gmail.com>
2023-12-17 01:17:34 -0500

    Add StaticImageData to useProjectPage hook

--- src/hooks/useProjectPage.ts
+++ src/hooks/useProjectPage.ts
@@ -1,7 +1,8 @@
 import { usePathname } from 'next/navigation';
 import { getProjectHeroSrc } from '#/utils';
+import { StaticImageData } from 'next/image';
 
-export function useProjectPage() {
+export function useProjectPage(): [boolean, StaticImageData] {
   const pathname = usePathname();
   const projectLink = pathname.split('/')[2];
   const isProjectPage = pathname.includes('/projects/');
