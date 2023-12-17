ef65293a6e0dbc7a43ca92b29ea31fbdb63e2c43
McCambley <jakemccamb16@gmail.com>
2023-12-17 01:17:53 -0500

    Refactor Logo component to dynamically adjust size based on scroll

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import sun from '~/sun.svg';
+import { useScroll } from '@/hooks/useScroll';
 
 const BUTTON_SIZE = 40;
-const LOGO_SIZE = 80;
 
 function Logo() {
+  const [logoSize, setLogoSize] = useState(80);
+  const [isScrolled] = useScroll();
+
+  useEffect(() => {
+    if (isScrolled) {
+      setLogoSize(40);
+    } else {
+      setLogoSize(80);
+    }
+  }, [isScrolled]);
   return (
     <Link href="/" className="flex transition-all hover:opacity-70 ">
       <Image
         src={sun.src}
         alt={''}
-        width={LOGO_SIZE}
-        height={LOGO_SIZE}
+        width={logoSize}
+        height={logoSize}
         className="cursor-pointer transition-all"
       ></Image>
 
-      <div className="hidden  flex-col items-center justify-center p-4 text-center transition-opacity gradient-text sm:flex">
+      <div
+        className={`hidden  flex-col items-center justify-center p-4 text-center transition-opacity gradient-text sm:flex ${
+          isScrolled ? 'sm:hidden' : ''
+        }`}
+      >
         <p className="font-handwriting text-4xl">Ashley Fife</p>
         <p className="text-sm">UI/UX Designer</p>
       </div>
