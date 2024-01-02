813a346536eb7573dfcdc6b7e46e9969cc8a652b
McCambley <jakemccamb16@gmail.com>
2024-01-01 22:31:36 -0500

    Update SectionIntro component styles

--- src/components/SectionIntro.tsx
+++ src/components/SectionIntro.tsx
@@ -10,14 +10,14 @@ function SectionIntro({ title, subtitle, children }: Props) {
   return subtitle ? (
     <div className="mb-12 w-full">
       <h1 className="mb-4 w-full text-center font-handwriting">{title}</h1>
-      <p className="mx-auto max-w-[840px] text-center text-3xl gradient-text">
+      <p className="mx-auto max-w-[840px] text-center text-2xl gradient-text">
         {subtitle}
       </p>
       {children}
     </div>
   ) : (
     <div className="mb-4 w-full">
-      <h2 className="mb-4 w-full text-center font-heading">{title}</h2>
+      <h2 className="mb-4 w-full text-center font-handwriting">{title}</h2>
       {children}
     </div>
   );
