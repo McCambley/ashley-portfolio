95384975f12301ee9ec9c581369bb2170eb016ee
McCambley <jakemccamb16@gmail.com>
2024-01-01 21:38:29 -0500

    Update Skill component styling

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -4,7 +4,7 @@ import GradientBorder from './GradientBorder';
 function Skill({ title, icon, details }: Skill) {
   return (
     <GradientBorder className="flex min-h-[27.5rem] w-full max-w-[360px] flex-col items-center justify-start rounded-md p-8">
-      <h3 className="mb-6 text-center font-heading text-3xl leading-none">
+      <h3 className="mb-6 text-center font-heading text-3xl leading-10">
         {title}
       </h3>
       <Image src={icon} alt={title} width={68} height={68} className="mb-5" />
