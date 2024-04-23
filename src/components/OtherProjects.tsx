c71d9747536616b7192d1a5b6cf85a48ab28fda3
McCambley <jakemccamb16@gmail.com>
2024-04-23 09:39:06 -0400

    Remove section intro from other projects

--- src/components/OtherProjects.tsx
+++ src/components/OtherProjects.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import OtherProject from './OtherProject';
 import { otherProjects } from '#/constants';
-import SectionIntro from './SectionIntro';
 
 const OtherProjects: React.FC = () => {
   return (
-    <section className="pad-top">
-      <SectionIntro title="Other Projects" subtitle={''} />
-      <div className="grid grid-cols-1 gap-x-4 gap-y-32 pt-10  md:grid-cols-2 lg:grid-cols-3">
-        {otherProjects.map((project, index) => {
-          return (
-            <OtherProject
-              key={project.title}
-              project={project}
-              // className={index === projects.length - 1 ? 'mb-0' : 'mb-20'}
-            />
-          );
-        })}
-      </div>
+    <section className="pad-top grid grid-cols-1 gap-x-4 gap-y-32 pb-32  md:grid-cols-2 lg:grid-cols-3">
+      {otherProjects.map((project, index) => {
+        return <OtherProject key={project.title} project={project} />;
+      })}
     </section>
   );
 };
