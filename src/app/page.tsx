aae3f116fd8d539c359fb71c661deb191cb7a957
McCambley <jakemccamb16@gmail.com>
2024-04-23 00:29:09 -0400

    Place other project on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Projects from '@/components/Projects';
 import Hero from '@/components/Hero';
 import ContactForm from '@/components/ContactForm';
+import OtherProjects from '@/components/OtherProjects';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects />
+      <OtherProjects />
       <ContactForm
         className="mb-16 max-w-[calc(800px+64px)]"
         standalone={false}
