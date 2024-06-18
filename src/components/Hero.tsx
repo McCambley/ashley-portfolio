2edaff64483b43b90e07d2b3bca7ed9e4c294305
McCambley <jakemccamb16@gmail.com>
2024-06-17 21:42:11 -0400

    Shorten the height of the hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,22 +4,22 @@ import GradientBorder from './GradientBorder';
 
 function Hero() {
   return (
-    <section className="pad-top mx-auto flex min-h-screen w-full flex-col items-center md:flex-row">
+    <section className="pad-top mx-auto flex min-h-[68vh] w-full flex-col items-center md:flex-row">
       <div className="mb-6 w-full md:w-1/2">
         {/* The only handwriting element around */}
         <h1 className="mb-4 w-full text-center font-handwriting md:mb-auto md:text-left">
           Hi! <span className="gradient-text">I&apos;m Ashley</span>
         </h1>
-        <GradientBorder className="mx-auto mb-6 max-w-fit rounded-full md:hidden">
-          <Image
-            src={ashleyHeadshot.src}
-            height={400}
-            width={400}
-            alt="Ashley in Alaska"
-            className="rounded-full md:hidden"
-            priority
-          />
-        </GradientBorder>
+        {/* <GradientBorder className=""> */}
+        <Image
+          src={ashleyHeadshot.src}
+          height={340}
+          width={340}
+          alt="Ashley in Alaska"
+          className="mx-auto mb-6 rounded-full md:hidden"
+          priority
+        />
+        {/* </GradientBorder> */}
         <p className="text-af-light text-center font-body text-2xl md:text-left">
           I&apos;m a UI/UX designer with a love for blending beauty and
           functionality. I aim to bring vibrant design and intuitive user
@@ -29,8 +29,8 @@ function Hero() {
       <div className="flex w-full place-content-center px-8 md:w-1/2">
         <Image
           src={ashleyHeadshot.src}
-          height={400}
-          width={400}
+          height={340}
+          width={340}
           alt="Ashley in Alaska"
           className="hidden rounded-full md:flex"
           priority
