312d373a1c9f75c7d1986ef8f7cf1e04f4d080cb
McCambley <jakemccamb16@gmail.com>
2024-06-22 10:45:53 -0400

    Polish burst component usability

--- src/components/BabyBurst.tsx
+++ src/components/BabyBurst.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 
 interface Props {
   pixels?: number;
+  burstStyle?: React.CSSProperties;
+  wrapperStyle?: React.CSSProperties;
+  translateX?: number;
+  translateY?: number;
+  inline?: boolean;
 }
 
-function BabyBurst({ pixels = 400 }: Props) {
-  const halfPixels = pixels / 2;
+function BabyBurst({
+  pixels = 400,
+  burstStyle = {},
+  wrapperStyle = {},
+  translateX = -50,
+  translateY = -50,
+  inline = false,
+}: Props) {
   return (
-    <span className="relative">
+    <span
+      className="relative inline w-full overflow-visible text-center"
+      style={{ display: inline ? 'inline' : 'inline-block', ...wrapperStyle }}
+    >
       <span
-        className={`linear-gradient absolute left-[-${halfPixels}px] top-[-${halfPixels}px] h-[${pixels}px] w-[${pixels}px] text-center`}
+        className="linear-gradient absolute text-center"
+        style={{
+          transform: `translate(${translateX}%, ${translateY}%)`,
+          width: `${pixels}px`,
+          height: `${pixels}px`,
+          ...burstStyle,
+        }}
       ></span>
     </span>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import ashleyHeadshot from '~/ashley_nh.png';
 import GradientBorder from './GradientBorder';
 import Link from 'next/link';
+import BabyBurst from './BabyBurst';
 
 function Hero() {
   return (
@@ -9,7 +10,8 @@ function Hero() {
       <div className="mb-6 w-full md:w-1/2">
         {/* The only handwriting element around */}
         <h1 className="mb-4 w-full text-center font-handwriting md:mb-auto md:text-left">
-          Hi! <span className="gradient-text">I&apos;m Ashley</span>
+          Hi! <BabyBurst inline translateY={-40} pixels={1000} />
+          <span className="gradient-text">I&apos;m Ashley</span>
         </h1>
         {/* <GradientBorder className=""> */}
         <Image
@@ -21,20 +23,20 @@ function Hero() {
           priority
         />
         {/* </GradientBorder> */}
-        <p className="text-af-light text-center font-body text-2xl md:text-left">
+        <p className="text-center font-body text-2xl text-af-light md:text-left">
           I&apos;m a UI/UX designer with a love for blending beauty and
           functionality. I aim to bring vibrant design and intuitive user
           experience to all facets of the human experience.
         </p>
-        <div className="text-af-light flex justify-center gap-4 pt-6 text-lg md:justify-start">
+        <div className="flex justify-center gap-4 pt-6 text-lg text-af-light md:justify-start">
           <Link
-            className=" border-af-light block rounded-full border px-8 py-2 text-center font-heading transition-all hover:opacity-70"
+            className=" block rounded-full border border-af-light px-8 py-2 text-center font-heading transition-all hover:opacity-70"
             href="#projects"
           >
             My Work
           </Link>
           <Link
-            className=" border-af-light block rounded-full border px-8 py-2 text-center font-heading transition-all hover:opacity-70"
+            className=" block rounded-full border border-af-light px-8 py-2 text-center font-heading transition-all hover:opacity-70"
             href="/contact"
           >
             Let&apos;s Chat
