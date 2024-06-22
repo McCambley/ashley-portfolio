40d41a3c2729192fc27a612a2175c7fc258d2bf4
McCambley <jakemccamb16@gmail.com>
2024-06-21 22:58:43 -0400

    Remove other projects display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Projects from '@/components/Projects';
 import Hero from '@/components/Hero';
 import ContactForm from '@/components/ContactForm';
-import OtherProjects from '@/components/OtherProjects';
+// import OtherProjects from '@/components/OtherProjects';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects />
-      <OtherProjects />
+      {/* <OtherProjects /> */}
       <ContactForm
         className="mb-16 max-w-[calc(800px+64px)]"
         standalone={false}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,12 @@
 import ProjectsComponent from '@/components/Projects';
 import ContactForm from '@/components/ContactForm';
-import OtherProjects from '@/components/OtherProjects';
+// import OtherProjects from '@/components/OtherProjects';
 
 function Projects() {
   return (
     <>
       <ProjectsComponent standalone={true} />
-      <OtherProjects />
+      {/* <OtherProjects /> */}
       <ContactForm
         className="mb-16 max-w-[calc(800px+64px)]"
         standalone={false}
