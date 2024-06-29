b4b9f1e26e81a56f6aa6a16207de8adbeff9522b
McCambley <jakemccamb16@gmail.com>
2024-06-29 14:44:53 -0400

    Update about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,22 +3,23 @@ import Image from 'next/image';
 import GradientBorder from '@/components/GradientBorder';
 import ContactForm from '@/components/ContactForm';
 import SectionIntro from '@/components/SectionIntro';
+import BabyBurst from '@/components/BabyBurst';
 
 const paragraphs = [
   {
     imageSrc: '/ashley_ak.png',
     alt: 'Ashley in Alaska',
-    text: `Through my experiences working in diverse settings and trades, I bring a unique perspective to design, branding, and user testing.`,
+    text: 'Through a lens of my diverse experiences, I bring a unique perspective to design, branding, and user testing.',
   },
   {
     imageSrc: '/ashley_greenland.png',
     alt: 'Ashley in Greenland',
-    text: `When I finished my bachelor's degree in Counseling Psychology, I longed to do unconventional jobs in unconventional places. I spent many years as a carpenter in New Hampshire, Greenland, Alaska, and Antarctica in the pursuit of land stewardship and supporting science.`,
+    text: 'When I finished my bachelors degree in Counseling Psychology, I longed to go to do unconventional jobs in unconventional places. I spent a decade doing seasonal manual labor in New Hampshire, Greenland, Alaska, and Antarctica in the pursuit of land stewardship and supporting science.',
   },
   {
     imageSrc: '/ashley_ice.png',
     alt: 'Ashley in Antarctica',
-    text: `I began to desire new horizons and was led to website design. I discovered parallels between the craft of carpentry and the art of UI/UX design. Both require creating a product that aligns with the client's vision and caters to the needs of those engaging with it. This transition from physical to digital landscapes ignited a newfound passion as I discovered a career that could seamlessly merge the world of technology with interests in science, conservation, and psychology.`,
+    text: 'After many years of ice and snow, I felt it was time to pursue a new horizon. The transition from physical to digital landscapes ignited a newfound passion. I found a career that could live between the world of tech and my passions such as science, conservation, or psychology.',
   },
 ];
 
@@ -28,11 +29,12 @@ export default function About() {
       <section className="pad-top">
         <SectionIntro
           title="About Me"
-          subtitle="UI/UX and Web Designer, Brand Elevator, and Outdoor Enthusiast"
+          onPage
+          // subtitle="UI/UX and Web Designer, Brand Elevator, and Outdoor Enthusiast"
         />
-        <div className="mx-auto flex max-w-[1000px] flex-col items-center rounded-lg p-4 md:p-8 lg:px-8">
+        <div className="flex w-full flex-col items-center rounded-lg">
           <div className="flex flex-col justify-between md:flex-row">
-            <div className="text-af-light flex w-full flex-col items-center md:w-1/2">
+            <div className="flex w-full flex-col items-center text-af-light md:w-1/2">
               {paragraphs.map(({ text, imageSrc, alt }) => (
                 <>
                   {/* <GradientBorder className=""> */}
@@ -55,9 +57,14 @@ export default function About() {
             </div>
             <div className="hidden w-full items-center justify-center md:flex md:w-1/2">
               <div className="relative  h-full max-h-[800px] w-full max-w-[400px] items-start justify-center">
-                <div className="absolute left-[50%] top-[0%]">
-                  {/* Top right */}
-                  {/* <GradientBorder className=""> */}
+                <BabyBurst translateX={-20} translateY={0} pixels={600} />
+                <Image
+                  src={'/about_pics.png'}
+                  alt="Ashley in the world"
+                  width={900}
+                  height={1600}
+                />
+                {/* <div className="absolute left-[60%] top-[0%]">
                   <Image
                     src={'/ashley_ice.png'}
                     alt="Ashley in Alaska"
@@ -65,23 +72,17 @@ export default function About() {
                     height={300}
                     className="h-auto rounded-full"
                   />
-                  {/* </GradientBorder> */}
                 </div>
                 <div className="absolute left-[0%] top-[27%]">
-                  {/* Middle left */}
-                  {/* <GradientBorder className=""> */}
                   <Image
                     src={'/ashley_ak.png'}
                     alt="Ashley in Alaska"
-                    width={300}
-                    height={300}
+                    width={250}
+                    height={250}
                     className="h-auto rounded-full"
                   />
-                  {/* </GradientBorder> */}
                 </div>
-                <div className="absolute left-[50%] top-[70%]">
-                  {/* Bottom right */}
-                  {/* <GradientBorder className=""> */}
+                <div className="absolute left-[60%] top-[70%]">
                   <Image
                     src={'/ashley_greenland.png'}
                     alt="Ashley in Alaska"
@@ -89,8 +90,7 @@ export default function About() {
                     height={300}
                     className="h-auto rounded-full"
                   />
-                  {/* </GradientBorder> */}
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
