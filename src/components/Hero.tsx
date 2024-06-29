069f43842c3b3f7c700130eb25d74f02d9a7b7ff
McCambley <jakemccamb16@gmail.com>
2024-06-29 14:42:43 -0400

    Update hero layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,26 +7,26 @@ import BabyBurst from './BabyBurst';
 function Hero() {
   return (
     <section className="pad-top mx-auto flex min-h-[100vh] w-full max-w-[1080px] flex-col items-center md:flex-row">
-      <div className="mb-6 w-full md:w-1/2">
-        <h1 className="mb-4 w-full text-center font-handwriting md:mb-8 md:text-left">
+      <div className="mb-6 w-full md:w-[55%]">
+        <h1 className="mb-2 w-full text-center font-handwriting md:mb-2 md:text-left">
           Hi!{' '}
           <BabyBurst inline translateY={-25} translateX={-50} pixels={600} />
           <span>I&apos;m Ashley</span>
         </h1>
         <Image
           src={ashleyHeadshot.src}
-          height={340}
-          width={340}
+          height={300}
+          width={300}
           alt="Ashley in Alaska"
           className="mx-auto mb-6 rounded-full md:hidden"
           priority
         />
-        <p className="text-center font-body text-2xl text-af-light md:mb-8 md:text-left">
+        <p className="mb-8 text-center font-body text-2xl text-af-light md:text-left">
           I&apos;m a UI/UX designer with a love for blending beauty and
           functionality. I aim to bring vibrant design and intuitive user
           experience to all facets of the human experience.
         </p>
-        <div className="flex justify-center gap-4 pt-6 text-af-light md:justify-start">
+        <div className="flex justify-center gap-4 text-af-light md:justify-start">
           <Link
             className=" block rounded-full border border-af-polar px-8 py-3 text-center font-heading transition-all hover:opacity-70"
             href="#projects"
@@ -41,12 +41,12 @@ function Hero() {
           </Link>
         </div>
       </div>
-      <div className="flex w-full place-content-center px-8 md:w-1/2">
+      <div className="flex w-full place-content-center px-8 md:w-[45%]">
         <BabyBurst translateX={-18} translateY={-16} pixels={600} />
         <Image
           src={ashleyHeadshot.src}
-          height={400}
-          width={400}
+          height={300}
+          width={300}
           alt="Ashley in Alaska"
           className="hidden rounded-full md:flex"
           priority
