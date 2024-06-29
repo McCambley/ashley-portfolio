276abcbb27cdb4ff51956a2132b7519dbc6d4f49
McCambley <jakemccamb16@gmail.com>
2024-06-29 14:44:18 -0400

    Change section intro layout

--- src/components/SectionIntro.tsx
+++ src/components/SectionIntro.tsx
@@ -5,24 +5,21 @@ interface Props {
   title: string;
   subtitle?: string;
   children?: React.ReactNode;
+  onPage?: boolean;
 }
 
-function SectionIntro({ title, subtitle, children }: Props) {
-  return subtitle ? (
+function SectionIntro({ title, subtitle, children, onPage }: Props) {
+  return subtitle || onPage ? (
     <div className="mb-12 w-full">
-      <h1 className="mb-4 w-full text-center font-heading font-medium">
-        {title}
-      </h1>
-      <p className="mx-auto max-w-[840px] text-center text-2xl text-af-light">
-        {subtitle}
-      </p>
+      <h1 className="mb-4 w-full font-heading text-5xl font-medium">{title}</h1>
+      {subtitle ? (
+        <p className="mx-auto w-full text-2xl text-af-light">{subtitle}</p>
+      ) : null}
       {children}
     </div>
   ) : (
     <div className="mb-4 w-full">
-      <h2 className="mb-4 w-full text-center font-heading font-medium">
-        {title}
-      </h2>
+      <h2 className="mb-4 w-full font-heading font-medium">{title}</h2>
       {children}
     </div>
   );
