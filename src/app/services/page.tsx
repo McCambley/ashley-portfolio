034284e7db4a6cb9d38e5fe519d1e5383b33f787
McCambley <jakemccamb16@gmail.com>
2024-06-29 16:04:31 -0400

    Update freelance layout

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -14,7 +14,7 @@ export default function Services() {
 passionate about crafting your stunning new website or giving your old
 site a modern remodel."
         >
-          <div className="flex w-full flex-col justify-center gap-5 pt-8 font-bold text-af-light sm:flex-row sm:gap-8 [&_a]:mx-auto [&_a]:w-full  [&_a]:min-w-[220px] sm:[&_a]:mx-0 sm:[&_a]:max-w-[240px]">
+          {/* <div className="flex w-full flex-col justify-center gap-5 pt-8 font-bold text-af-light sm:flex-row sm:gap-8 [&_a]:mx-auto [&_a]:w-full  [&_a]:min-w-[220px] sm:[&_a]:mx-0 sm:[&_a]:max-w-[240px]">
             <Link
               href={'#contact'}
               className="white-border flex items-center justify-center rounded-full px-7 py-2 font-heading text-lg text-af-light  transition-all hover:opacity-70"
@@ -26,27 +26,30 @@ site a modern remodel."
                 View my work
               </span>
             </Link>
-          </div>
+          </div> */}
         </SectionIntro>
-        <GradientBorder className="mb-8 flex w-full flex-col items-center justify-center rounded-lg p-4 md:p-10">
-          <p className="mb-4 w-full text-center font-heading text-4xl text-af-light">
-            Specialization
-          </p>
-          <Image
-            src="/services_target.svg"
-            width={75}
-            height={75}
-            alt="Target"
-            className="mb-4"
-          />
-          <p className="text-center text-2xl text-af-light">
-            My expertise includes Website Design, UI/UX Design, Branding, and
-            User Experience optimization to bring more customers to your site.
-          </p>
-        </GradientBorder>
         <div className=" flex flex-col justify-center gap-8 lg:flex-row">
-          <GradientBorder className="flex w-full flex-col justify-start rounded-lg p-4 md:p-10">
-            <p className="mb-4 w-full text-center font-heading text-4xl text-af-light">
+          <div className="mb-8 flex w-full flex-col items-center justify-start rounded-lg p-4 md:p-3">
+            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
+              Specialization
+            </p>
+            <Image
+              src="/services_target.svg"
+              width={75}
+              height={75}
+              alt="Target"
+              className="mx-auto mb-10"
+            />
+            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+              <li>Website Design</li>
+              <li>UI/UX Design</li>
+              <li>Branding</li>
+              <li>User Experience</li>
+              <li>I aim to optimize your site to attract more users</li>
+            </ul>
+          </div>
+          <div className="flex w-full flex-col justify-start rounded-lg p-4 md:p-3">
+            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
               What to Expect
             </p>
             <Image
@@ -54,34 +57,35 @@ site a modern remodel."
               width={75}
               height={75}
               alt="Target"
-              className="mx-auto mb-4"
+              className="mx-auto mb-10"
             />
-            <p className="text-center text-2xl text-af-light lg:text-left">
-              You can expect clear communication, timely updates, and a
-              high-quality end product. I prioritize client satisfaction and
-              will work closely with you to ensure that your project meets your
-              expectations.
-            </p>
-          </GradientBorder>
-          <GradientBorder className="flex w-full flex-col justify-start rounded-lg p-4 md:p-10">
-            <p className="mb-4 w-full text-center font-heading text-4xl text-af-light">
-              Payment
+            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+              <li>Clear communication</li>
+              <li>Timely updates</li>
+              <li>High-quality end product</li>
+              <li>client satisfaction</li>
+            </ul>
+          </div>
+          <div className="flex w-full flex-col justify-start rounded-lg p-4 md:p-3">
+            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
+              How to Pay Me
             </p>
             <Image
               src="/services_payment.svg"
               width={75}
               height={75}
               alt="Target"
-              className="m-auto mb-4"
+              className="mx-auto mb-10"
             />
-            <p className="text-center text-2xl text-af-light lg:text-left">
-              I accept payments through various methods, including Venmo, bank
-              transfers, and checks. Once we agree on the project scope and
-              cost, I&apos;ll provide you with an invoice. Payment terms and
-              methods will be discussed and agreed upon before the project
-              begins.
-            </p>
-          </GradientBorder>
+            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+              <li>Venmo, cash, and check</li>
+              <li>Invoice provided after project agreement has been signed</li>
+              <li>
+                Payment terms and methods will be discussed and agreed upon
+                before the project begins
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
       <ContactForm
