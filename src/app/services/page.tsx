5e2fbc3346db6e2cfe67b8ec39d4b2d7db7c0a5d
McCambley <jakemccamb16@gmail.com>
2024-07-02 23:57:54 -0400

    Position bursts in services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -30,66 +30,113 @@ site a modern remodel."
             </Link>
           </div>
         </SectionIntro>
+        <BabyBurst
+          width={500}
+          height={500}
+          translateY={0}
+          translateX={-50}
+          wrapperStyle="xl:hidden"
+        />
+        <BabyBurst
+          width={500}
+          height={500}
+          translateY={138}
+          translateX={-50}
+          wrapperStyle="xl:hidden"
+        />
+        <BabyBurst
+          width={500}
+          height={500}
+          translateY={268}
+          translateX={-50}
+          wrapperStyle="xl:hidden"
+        />
         <div className="flex flex-col justify-center gap-10 pt-12 xl:flex-row [&_div]:min-w-[332px] [&_div]:px-9 [&_div]:py-16 [&_ul]:ml-3 ">
-          <BabyBurst />
-          <div className="mb-8 flex w-full flex-col justify-start rounded-xl bg-af-dark">
-            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
-              Specialization
-            </p>
-            <Image
-              src="/services_target.svg"
-              width={75}
-              height={75}
-              alt="Target"
-              className="mx-auto mb-10"
+          <div className="w-full">
+            <BabyBurst
+              width={336}
+              height={336}
+              translateY={-15}
+              wrapperStyle="hidden xl:block"
             />
-            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
-              <li>Website Design</li>
-              <li>UI/UX Design</li>
-              <li>Branding</li>
-              <li>User Experience</li>
-              <li>I aim to optimize your site to attract more users</li>
-            </ul>
+
+            <div className="flex w-full flex-col justify-start rounded-xl bg-af-dark">
+              <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
+                Specialization
+              </p>
+              <Image
+                src="/services_target.svg"
+                width={75}
+                height={75}
+                alt="Target"
+                className="mx-auto mb-10"
+              />
+              <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+                <li>Website Design</li>
+                <li>UI/UX Design</li>
+                <li>Branding</li>
+                <li>User Experience</li>
+                <li>I aim to optimize your site to attract more users</li>
+              </ul>
+            </div>
           </div>
-          <BabyBurst />
-          <div className="flex w-full flex-col justify-start  rounded-xl bg-af-dark">
-            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
-              What to Expect
-            </p>
-            <Image
-              src="/services_handshake.svg"
-              width={75}
-              height={75}
-              alt="Target"
-              className="mx-auto mb-10"
+          <div className="w-full">
+            <BabyBurst
+              width={336}
+              height={336}
+              translateY={-15}
+              wrapperStyle="hidden xl:block"
             />
-            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
-              <li>Clear communication</li>
-              <li>Timely updates</li>
-              <li>High-quality end product</li>
-              <li>client satisfaction</li>
-            </ul>
+
+            <div className="flex w-full flex-col justify-start  rounded-xl bg-af-dark">
+              <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
+                What to Expect
+              </p>
+              <Image
+                src="/services_handshake.svg"
+                width={75}
+                height={75}
+                alt="Target"
+                className="mx-auto mb-10"
+              />
+              <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+                <li>Clear communication</li>
+                <li>Timely updates</li>
+                <li>High-quality end product</li>
+                <li>client satisfaction</li>
+              </ul>
+            </div>
           </div>
-          <BabyBurst />
-          <div className="flex w-full flex-col justify-start rounded-xl bg-af-dark">
-            <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
-              How to Pay Me
-            </p>
-            <Image
-              src="/services_payment.svg"
-              width={75}
-              height={75}
-              alt="Target"
-              className="mx-auto mb-10"
+          <div className="w-full">
+            <BabyBurst
+              width={336}
+              height={336}
+              translateY={-15}
+              wrapperStyle="hidden xl:block"
             />
-            <ul className="list-disc pl-4 text-2xl font-light text-af-light">
-              <li>Venmo, cash, and check</li>
-              <li>Invoice provided after project agreement has been signed</li>
-              <li>
-                Payment terms and methods will be discussed and agreed upon
-                before the project begins
-              </li>
-            </ul>
+
+            <div className="flex w-full flex-col justify-start rounded-xl bg-af-dark">
+              <p className="mb-4 min-h-[80px] w-full text-center font-heading text-4xl text-af-light">
+                How to Pay Me
+              </p>
+              <Image
+                src="/services_payment.svg"
+                width={75}
+                height={75}
+                alt="Target"
+                className="mx-auto mb-10"
+              />
+              <ul className="list-disc pl-4 text-2xl font-light text-af-light">
+                <li>Venmo, cash, and check</li>
+                <li>
+                  Invoice provided after project agreement has been signed
+                </li>
+                <li>
+                  Payment terms and methods will be discussed and agreed upon
+                  before the project begins
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
