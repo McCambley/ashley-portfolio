41fe1c8ce807593c7633c72ded596e1c225f7787
McCambley <jakemccamb16@gmail.com>
2024-07-03 00:41:37 -0400

    Create progress with burst component

---
+++ src/components/ProgressWithBurst.tsx
@@ -0,0 +1,18 @@
+import BabyBurst from '@/components/BabyBurst';
+import StaticImage from '@/components/StaticImage';
+import { StaticImageData } from 'next/image';
+
+export default function ProgressWithBurst({
+  image,
+  alt,
+}: {
+  image: StaticImageData;
+  alt: string;
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <BabyBurst translateX={-50} />
+      <StaticImage className="project-progress" image={image} alt={alt} />
+    </div>
+  );
+}
