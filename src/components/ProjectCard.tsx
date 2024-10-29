9773256a422b92326429b4235d9ec81459cb7384
McCambley <jakemccamb16@gmail.com>
2024-10-28 23:36:28 -0400

    Hide overlay on small screens

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,30 +8,42 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
-    <Link
-      target={project.target}
-      href={project.link ? `/projects/${project.link}` : project.externalLink}
-      className="relative aspect-square overflow-hidden rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl"
-    >
-      <Image
-        width={400}
-        height={400}
-        src={project.src}
-        alt={project.title}
-        className="object-cover"
-      />
-      <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-100 bg-opacity-50 px-7 py-16 opacity-0 opacity-90 transition-opacity duration-150">
-        <div className="flex h-full flex-col justify-center text-center text-af-dark">
-          <h3 className="min-h-[80px] font-heading text-4xl font-bold text-af-dark">
-            {project.title}
-          </h3>
-          <p className="mb-8 text-xl text-af-dark">{project.description}</p>
-          <p className="font-bold text-af-dark transition-opacity hover:opacity-70">
-            <span className="underline">{project.callToAction}</span> &gt;
-          </p>
+    <div>
+      <Link
+        target={project.target}
+        href={project.link ? `/projects/${project.link}` : project.externalLink}
+        className="relative aspect-square overflow-hidden rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl"
+      >
+        <Image
+          width={800}
+          height={800}
+          src={project.src}
+          alt={project.title}
+          className="object-cover"
+        />
+        <div className="absolute inset-0 flex hidden items-center justify-center bg-white bg-opacity-50 px-7 py-16 opacity-0 transition-opacity duration-150 hover:bg-opacity-100 hover:opacity-90 sm:block">
+          <div className="flex h-full flex-col justify-center text-center text-af-dark">
+            <h3 className="min-h-[80px] font-heading text-4xl font-bold text-af-dark">
+              {project.title}
+            </h3>
+            <p className="mb-8 text-xl text-af-dark">{project.description}</p>
+            <p className="font-bold text-af-dark transition-opacity hover:opacity-70">
+              <span className="underline">{project.callToAction}</span> &gt;
+            </p>
+          </div>
         </div>
+      </Link>
+      <div className="pb-8 pt-4 sm:hidden">
+        <Link
+          href={
+            project.link ? `/projects/${project.link}` : project.externalLink
+          }
+        >
+          <h3 className="mb-1 underline">{project.title}</h3>
+        </Link>
+        <p>{project.description}</p>
       </div>
-    </Link>
+    </div>
   );
 };
 
