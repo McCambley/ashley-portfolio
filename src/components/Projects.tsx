c6cd7d6bd13d6af0d8a073bc08495b84761bc62c
McCambley <jakemccamb16@gmail.com>
2025-01-05 21:13:21 -0500

    Refactor dead code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,24 +13,6 @@ export function Projects({ standalone = false }: { standalone?: boolean }) {
       className={standalone ? 'pad-top' : 'py-9 md:py-24 md:pt-0'}
       id="projects"
     >
-      {/* {standalone ? (
-        <SectionIntro
-          title="My Work"
-          subtitle="A Fusion of Sophisticated Design, Aesthetics, and Impactful Creativity"
-        />
-      ) : null} */}
-      {/* {projects.map((project, index) => {
-        return (
-          <Project
-            key={project.title}
-            project={project}
-            isReverse={Boolean(index % 2)}
-            className={`${
-              index === projects.length - 1 ? 'lg:mb-0' : 'mb-8 lg:mb-[200px]'
-            }`}
-          />
-        );
-      })} */}
       <ProjectCardGrid title="Case Studies" projects={caseStudies} />
       <ProjectCardGrid title="Other Work" projects={otherWork} />
     </section>
